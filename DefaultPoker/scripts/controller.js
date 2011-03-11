6fe837c495dae8120b76e500c93d63d1a6d901d8
andres.saaremots@gmail.com <andres.saaremots@gmail.com@4840448a-4cad-dbae-53e2-816ae8611d70>
2011-03-11 09:41:58 +0000

    controller impl

--- DefaultPoker/scripts/controller.js
+++ DefaultPoker/scripts/controller.js
@@ -1,4 +1,30 @@
-/**
+
+/*
+ * controller.js
+ * 
+ * GUI calls these functions on user interaction
  * 
  */
 
+var engine; // engine.js
+var animation; // animation.js
+var history; // array of ended game objects
+
+/**
+ * called in <body onload="...
+ * all startup code goes here
+ * creates animation object
+ */
+function pageLoaded() {
+	animation = new Animation();
+}
+
+/**
+ * called when new game button clicked
+ * engine restarted
+ * old engine pushed to stack
+ */
+function startNewGameClicked() {
+	history.push(engine);
+	engine = new Engine();
+}
