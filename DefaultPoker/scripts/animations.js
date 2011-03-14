3cdac1308e745dfb6072a109a122b34d32a4950d
ardityrk@gmail.com <ardityrk@gmail.com@4840448a-4cad-dbae-53e2-816ae8611d70>
2011-03-14 11:39:43 +0000

    Animatsioon kaartide jagamine

--- DefaultPoker/scripts/animations.js
+++ DefaultPoker/scripts/animations.js
@@ -6,13 +6,36 @@ $(document).ready(function(){
 
 	$("#newgamebutton").click(function(){
 	  
-		$("#dealoutcard1").animate({opacity: "1", left: "+=239", top: "-=21", height: "-=28", width: "-=21"}, 1000).delay(1000);		
-		$("#dealoutcard2").animate({opacity: "1", left: "+=210", top: "-=30", height: "-=28", width: "-=21"}, 1000).delay(1000);
+		//Player7
+		$("#dealoutcard1").animate({opacity: "1", left: "+=239", top: "-=21", height: "-=28", width: "-=21"}, 1000);		
+		$("#dealoutcard2").delay(300).animate({opacity: "1", left: "+=210", top: "-=30", height: "-=28", width: "-=21"}, 1000);
+		//Player6
+		$("#dealoutcard3").delay(900).animate({opacity: "1", left: "+=178", top: "-=220", height: "-=28", width: "-=21"}, 1000);
+		$("#dealoutcard4").delay(1200).animate({opacity: "1", left: "+=148", top: "-=230", height: "-=28", width: "-=21"}, 1000);
+		//Player5
+		$("#dealoutcard5").delay(1500).animate({opacity: "1", left: "-=3", top: "-=250", height: "-=28", width: "-=21"}, 1000);
+		$("#dealoutcard6").delay(1800).animate({opacity: "1", left: "-=32", top: "-=262", height: "-=28", width: "-=21"}, 1000);
+		//Player4
+		$("#dealoutcard7").delay(2100).animate({opacity: "1", left: "-=202", top: "-=250", height: "-=28", width: "-=21"}, 1000);
+		$("#dealoutcard8").delay(2400).animate({opacity: "1", left: "-=233", top: "-=262", height: "-=28", width: "-=21"}, 1000);
+		//Player3
+		$("#dealoutcard9").delay(2700).animate({opacity: "1", left: "-=403", top: "-=220", height: "-=28", width: "-=21"}, 1000);
+		$("#dealoutcard10").delay(3000).animate({opacity: "1", left: "-=433", top: "-=231", height: "-=28", width: "-=21"}, 1000);
+		//Player2
+		$("#dealoutcard11").delay(3300).animate({opacity: "1", left: "-=475", top: "-=21", height: "-=28", width: "-=21"}, 1000);
+		$("#dealoutcard12").delay(3700).animate({opacity: "1", left: "-=504", top: "-=30", height: "-=28", width: "-=21"}, 1000);
+		//Player1
+		$("#dealoutcard13").delay(4000).animate({opacity: "1", left: "-=403", top: "+=180", height: "-=28", width: "-=21"}, 1000);
+		$("#dealoutcard14").delay(4300).animate({opacity: "1", left: "-=432", top: "+=171", height: "-=28", width: "-=21"}, 1000);
+		//Player8
+		$("#dealoutcard15").delay(4600).animate({opacity: "1", left: "+=178", top: "+=180", height: "-=28", width: "-=21"}, 1000);
+		$("#dealoutcard16").delay(4900).animate({opacity: "1", left: "+=148", top: "+=171", height: "-=28", width: "-=21"}, 1000);
 		
-		
-		//$("#dealoutcard").animate({opacity: "1", left: "+=110", top: "-=30", height: "-=28", width: "-=21"}, 1000);
-		
-		//$("#tablecard5").animate({ right: "+=100" }, 1000);
+				
+		$("#dealoutcard17").delay(5200).animate({ left: "-=80" }, 1000);
+		$("#dealoutcard18").delay(5500).animate({ left: "-=160" }, 1000);
+		$("#dealoutcard19").delay(5800).animate({ left: "-=240" }, 1000);
+		$("#dealoutcard20").delay(6100).animate({ left: "-=320" }, 1000);
 	  
 		return false;
 	
