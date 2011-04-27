104cabf77ec24f7a7309e7803633c959f87c089a
ardityrk@gmail.com <ardityrk@gmail.com@4840448a-4cad-dbae-53e2-816ae8611d70>
2011-04-27 09:50:57 +0000

    Player.java implementeerimine

--- trunk/DefaultPokerWeb/src/ee/ut/defaultpoker/model/Player.java
+++ trunk/DefaultPokerWeb/src/ee/ut/defaultpoker/model/Player.java
@@ -1,5 +1,53 @@
 package ee.ut.defaultpoker.model;
 
-public class Player {
-
+public abstract class Player {
+	private String name;
+	private boolean is_bot;
+	private int chips;
+	private int bet=0;
+	private int turn=0;
+	private boolean dealer;
+	private boolean fold;
+	private int seat;
+	String[] cards;
+	
+	public void setCards(String[] cards) {
+		this.cards = cards;
+	}
+	
+	public String[] getCards() {
+		return this.cards;
+	}
+	
+	public void addChips(int amount) {
+		this.chips+=amount;
+	}
+	
+	public void reduceChips(int amount) {
+		this.chips-=amount;
+	}
+	
+	public int getChips() {
+		return this.chips;
+	}
+	
+	public int getBet() {
+		return this.bet;
+	}
+	
+	public void setBet(int amount) {
+		this.bet = amount;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String setName) {
+		this.name=setName;
+	}
+	
+	
+	
 }
+
